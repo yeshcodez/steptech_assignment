d71e50159490cbca8340a7ea33014c8f40e58114
yeshcodez <112760748+yeshcodez@users.noreply.github.com>
2023-09-09 07:57:23 +0530

    Add files via upload
    string return hello world

---
+++ app1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello world.'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
